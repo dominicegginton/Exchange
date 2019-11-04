e8bd7d62be2d6be6dd697f83fcd5b45291635aea
Dominic Egginton <eggintod@coventry.ac.uk>
2019-11-04 16:46:55 +0000

    added simple register route

--- services/user/routes/register.js
+++ services/user/routes/register.js
@@ -6,4 +6,8 @@ const Router = require('koa-router')
 /* SETUP ROUTER */
 const router = new Router()
 
+router.get('/register', async ctx => {
+	await ctx.render('register')
+})
+
 module.exports = router
