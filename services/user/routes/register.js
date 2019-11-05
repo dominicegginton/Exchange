2694acbc1a2062e91db8e71d55665192059ccddc
Dominic Egginton <eggintod@coventry.ac.uk>
2019-11-05 19:23:34 +0000

    added post route for registing user

--- services/user/routes/register.js
+++ services/user/routes/register.js
@@ -2,12 +2,26 @@
 
 /* IMPORT MODULES */
 const Router = require('koa-router')
+const User = require('../modules/user')
 
 /* SETUP ROUTER */
 const router = new Router()
 
 router.get('/register', async ctx => {
 	await ctx.render('register')
+})
+
+router.post('/register', async ctx => {
+	try {
+		const newUser = ctx.request.body
+		const newUserAvatar = ctx.request.files.avatar
+		const user = await new User()
+		const newUserID = await user.register(newUser)
+		await user.uploadAvatar(newUserID, newUserAvatar)
+		await ctx.redirect('/')
+	} catch (error) {
+		await ctx.render('register', {error: error})
+	}
 })
 
 module.exports = router
