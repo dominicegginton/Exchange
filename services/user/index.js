c7d24ecae565e90be29ef0ebc6a067eec4121dc3
Dominic Egginton <eggintod@coventry.ac.uk>
2019-11-07 16:21:50 +0000

    added new avatar router to serve user avatars to client

--- services/user/index.js
+++ services/user/index.js
@@ -6,8 +6,9 @@ const Compose = require('koa-compose')
 /* IMPORT ROUTERS */
 const registerRouter = require('./routes/register')
 const loginRouter = require('./routes/login')
+const avatarRouter = require('./routes/avatar')
 
 /* COMPOSE ROUTERS */
-const Router = Compose([registerRouter.routes(), loginRouter.routes()])
+const Router = Compose([registerRouter.routes(), loginRouter.routes(), avatarRouter.routes()])
 
 module.exports = Router
