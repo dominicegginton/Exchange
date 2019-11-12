da0ae5a8bd884da4e6c977d98b3ec9fa9fb01b7b
Dominic <eggintod@coventry.ac.uk>
2019-11-12 21:15:57 +0000

    created api endpoint for the item service and added a myItems route

--- services/item/index.js
+++ services/item/index.js
@@ -6,9 +6,10 @@ const Compose = require('koa-compose')
 /* IMPORT ROUTERS */
 const detailsRouter = require('./routes/details')
 const newRouter = require('./routes/new')
+const apiRouter = require('./routes/api')
 const imageRouter = require('./routes/image')
 
 /* COMPOSE ROUTERS */
-const Router = Compose([detailsRouter.routes(), newRouter.routes(), imageRouter.routes()])
+const Router = Compose([detailsRouter.routes(), newRouter.routes(), apiRouter.routes(), imageRouter.routes()])
 
 module.exports = Router
