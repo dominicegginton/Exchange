70d23060e3c8d4965b4e14104835eadb087f55c6
Dominic <eggintod@coventry.ac.uk>
2019-11-14 18:54:44 +0000

    added api router to handle ajax rejects for wishlist data

--- services/wishlist/index.js
+++ services/wishlist/index.js
@@ -5,8 +5,9 @@ const Compose = require('koa-compose')
 
 /* IMPORT ROUTERS */
 const newRouter = require('./routes/new')
+const apiRouter = require('./routes/api')
 
 /* COMPOSE ROUTERS */
-const Router = Compose([newRouter.routes()])
+const Router = Compose([newRouter.routes(), apiRouter.routes()])
 
 module.exports = Router
