67897f42b1825806c8df37b06704825c94fb6c23
Dominic <eggintod@coventry.ac.uk>
2019-11-14 18:39:57 +0000

    added wishlist service

---
+++ services/wishlist/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/* IMPORT KOA COMPOSE */
+const Compose = require('koa-compose')
+
+/* IMPORT ROUTERS */
+
+/* COMPOSE ROUTERS */
+const Router = Compose()
+
+module.exports = Router
