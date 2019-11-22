85b28d085202bec8941f9d05bf027e2148069b44
Dominic <eggintod@coventry.ac.uk>
2019-11-22 10:22:49 +0000

    added connection pool to mock of pg

--- __mocks__/pg.js
+++ __mocks__/pg.js
@@ -5,11 +5,10 @@ const SQLite = require('sqlite-async')
 class Client {
 
 	constructor() {
-		return this
-	}
-
-	async connect() {
-		this.database = await SQLite.open(':memory:')
+		return (async() => {
+			this.database = await SQLite.open(':memory:')
+			return this
+		})()
 	}
 
 	async query(sql) {
@@ -17,6 +16,22 @@ class Client {
 		return {rows: data}
 	}
 
+	async release() {
+		this.database = null
+	}
+
+}
+
+class Pool {
+
+	constructor() {
+		return this
+	}
+
+	async connect() {
+		return await new Client()
+	}
 }
 
 module.exports.Client = Client
+module.exports.Pool = Pool
